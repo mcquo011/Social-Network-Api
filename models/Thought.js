38d0264ad3a5b9ce323f99744e55b39b4ad7b41c
mcquo011 <omcquown@gmail.com>
2023-04-13 10:36:16 -0500

    Added thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+
+const thoughtsScehma = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema]   
+});
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtsScehma);
+
+module.exports = Thought;
