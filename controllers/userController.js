fd5b965bd3569aef8fa22f02329e0981d20a280c
mcquo011 <omcquown@gmail.com>
2023-04-16 15:49:19 -0500

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,42 @@ const userController = {
       res.status(500).json(err);
     }
   },
+  createUser: async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+      res.json(newUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-}
+
+  getUserById: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  updateUser: async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      res.json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  deleteUser: async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.json({ message: 'User deleted' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
 
 module.exports = userController;
